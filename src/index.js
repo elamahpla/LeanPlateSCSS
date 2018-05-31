bdfd417b6cef15f602ca24a592f8cea46dd8a4d6
elamahpla <theamazingness@gmail.com>
2018-05-31 12:32:19 -0500

    Updating file pathing. Adding/Removing stuff that is and isn't needed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './Containers/Hello';
-import './Styles/styles.scss';
+import '../css/style.css';
 import 'normalize.css';
+
+//This should be your Render file. The name index.js is just a placeholder. 
 
 ReactDOM.render(<Hello />, document.getElementById('app'));
 
